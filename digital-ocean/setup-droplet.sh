32d2214f772bedaf8c0d2964b2089c803f0deb98
Joeri Abbo <joeriabbo@hotmail.com>
2022-10-25 21:40:34 +0200

    Add setup nexus

--- digital-ocean/setup-droplet.sh
+++ digital-ocean/setup-droplet.sh
@@ -4,3 +4,16 @@ sudo apt upgrade -y
 sudo apt install openjdk-8-jre-headless htop nodejs docker net-tools -y
 adduser jabbo
 usermod -aG sudo jabbo
+
+cd /opt
+wget https://download.sonatype.com/nexus/3/latest-unix.tar.gz
+tar -zxvf latest-unix.tar.gz
+adduser nexus
+chown -R nexus:nexus nexus-3.42.0-01
+chown -R nexus:nexus sonatype-work
+vim nexus-3.42.0-01/bin/nexus.rc
+run_as_user="nexus"
+su - nexus
+/opt/nexus-3.42.0-01/bin/nexus start
+ps aux | grep nexus
+netstat -lnpt
